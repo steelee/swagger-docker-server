6760edcc599ee013f1880900789d817b3fd1cfcf
Evan Steele <esteele101@gmail.com>
2016-06-30 22:47:19 +0000

    Added error checking for adding to database - check for valid json and yaml extensions - check for successful cURL of document

--- dist/api/add.php
+++ dist/api/add.php
@@ -1,4 +1,6 @@
 <?php
+
+require_once "Spyc.php";
 $servername = "172.17.0.2";
 $username = "root";
 $password = "123";
@@ -13,11 +15,31 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-$sql = '"INSERT INTO api (url, name) VALUES ("' . $_POST['url'] . '","someapi")"';
+
+$file_parts = pathinfo($_POST['url']);
+if ($file_parts['extension'] != "yaml" and $file_parts['extension'] != "json"){ 
+	echo 1;
+	exit();
+}
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $_POST['url']
+			));
+
+$result = spyc_load_file(curl_exec($curl));
+if (sizeof($result) == 0){
+	echo 2;
+	exit();
+}
+
+$sql = 'INSERT INTO api (url, name) VALUES ("' . $_POST['url'] . '","someapi")';
 echo $sql;
 if ($conn->query($sql) === TRUE) {
 	echo "New record created successfully";
 } else {
 	echo "Error: " . $sql . "<br>" . $conn->error;
-}  
+}
+
 ?>
