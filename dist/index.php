15b668d417b138a9a79052113a10d52f869c8b39
steelee <esteele101@gmail.com>
2016-07-07 22:30:10 +0000

    Continued development of alternative landing page: - got consistent margins on swagger container - removed button to switch back to remote file - still need to implement minimap (feature for scrolling page)

--- dist/index.php
+++ dist/index.php
@@ -41,7 +41,27 @@
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main swagger-ui-wrap">
 		<div id="swagger-ui-container" class="swagger-ui-wrap"></div>
-
+    <div id="add_api_form" class="hidden">
+        <form id='api_selector' method="post" name="api_selector" class="form-inline">
+            <div class='form-group'>
+                <label for="api">API URL:</label>
+                <input id="api" placeholder="http://example.com/api" type=
+                "text">
+            </div><input type="submit" value="Add API">
+        <div class="clearfix"></div>
+        <div class="form-group">
+        Or select a file from your computer:  <button id="local" type="button">Use Local File</button> 
+        </div>
+        </form>
+        <div id="local_selector" style="display:none">
+            <form action="api/upload.php" enctype="multipart/form-data" id=
+            "local_selector" method="post" name="local_selector">
+                <div class='input'>
+                    <input id="upfile" name="upfile" type="file">
+                </div><input type="submit" value="Add API">
+            </form>
+        </div>
+    </div>
 
         </div>
       </div>
