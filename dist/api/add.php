b54676a0caf300a0f41ee16a7d4e18e396a88f10
steelee <esteele101@gmail.com>
2016-07-13 18:59:56 +0000

    Improved backend scripts and forms for adding APIs - changed add.php script to try-catch model like upload.php - removed old stylings from screen.css - moved <div> tags from index to header and footer files
    Signed-off-by: steelee <esteele101@gmail.com>

--- dist/api/add.php
+++ dist/api/add.php
@@ -5,37 +5,49 @@ require_once "secrets.php";
 
 $myArray = array();
 
-// Create connection
-$conn = new mysqli($DB_SERVER, $DB_NAME, $DB_PASS, $DB_SELECT);
+try {
 
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
+	// Create connection
+	$conn = new mysqli($DB_SERVER, $DB_NAME, $DB_PASS, $DB_SELECT);
 
-$file_parts = pathinfo($_POST['url']);
-if ($file_parts['extension'] != "yaml" and $file_parts['extension'] != "json"){ 
+	// Check connection
+	if ($conn->connect_error) {
+		throw new RuntimeException('Database connection error');
+	}
+	
+	$file_parts = pathinfo($_POST['url']);
+	if ($file_parts['extension'] != "yaml" and $file_parts['extension'] != "json"){ 
+		throw new RuntimeException('Invalid URL');
+	}
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_URL => $_POST['url']
+				));
+
+	$result = spyc_load_file(curl_exec($curl));
+	if (sizeof($result) == 0){
+		throw new RuntimeException('Failed to open remote URL');
+	}
+
+	if (sizeof($result) > 1000000) {
+		throw new RuntimeException('File size exceeded.');
+	}
+
+	$name = rtrim(basename($_POST['url'], $file_parts['extension']),'.');
+
+	$sql = 'INSERT INTO api (url, name) VALUES ("' . $_POST['url'] . '", "' . $name . '");'; 
+	if ($conn->query($sql) === TRUE) {
+		$_SESSION['status'] = "File successfully added";
+	} else {
+		throw new RuntimeException('Database write error');
+	}
+
+} catch (RuntimeException $e) {
+
+	$_SESSION['status'] = $e->getMessage();
+	header('Location: /');
 	exit();
-}
-$curl = curl_init();
-curl_setopt_array($curl, array(
-			CURLOPT_RETURNTRANSFER => 1,
-			CURLOPT_URL => $_POST['url']
-			));
-
-$result = spyc_load_file(curl_exec($curl));
-if (sizeof($result) == 0){
-	echo 2;
-	exit();
-}
-
-$name = rtrim(basename($_POST['url'], $file_parts['extension']),'.');
-
-$sql = 'INSERT INTO api (url, name) VALUES ("' . $_POST['url'] . '", "' . $name . '");'; 
-if ($conn->query($sql) === TRUE) {
-	echo "New record created successfully";
-} else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 ?>
--- dist/index.php
+++ dist/index.php
@@ -1,25 +1,35 @@
 <?php include 'header.htm';?>
-		<div id="swagger-ui-container" class="swagger-ui-wrap"></div>
-    <div id="add_api_form" class="hidden">
-        <form id='api_selector' method="post" name="api_selector" class="form-inline">
-            <div class='form-group'>
-                <label for="api">API URL:</label>
-                <input id="api" placeholder="http://example.com/api" type=
-                "text">
-            </div><input type="submit" value="Add API">
-        <div class="clearfix"></div>
-        </form>
-        <div id="local_selector">
-            <form action="api/upload.php" enctype="multipart/form-data" id=
-            "local_selector" method="post" name="local_selector">
-                <div class='input'>
-                    <input id="upfile" name="upfile" type="file">
-                </div><input type="submit" value="Add API">
-            </form>
-        </div>
-    </div>
-
-        </div>
+<div id="swagger-ui-container" class="swagger-ui-wrap"></div>
+<div id="add_api_form" class="hidden">
+    <h4>Add an API through a remote link or upload a file from your computer</h4>
+     <form id='api_selector' method="post" name="api_selector">
+     <div class="form-group ">
+      <label class="control-label requiredField">
+       URL
+      </label>
+      <input class="form-control" id="url" name="url" placeholder="http://example.com/file.yaml" type="text"/>
+     </div>
+     <div class="form-group">
+      <div>
+       <button class="btn btn-primary " name="submit" type="submit">
+        Submit Link
+       </button>
       </div>
-    </div>
+     </div>
+    </form>
+     <form action="api/upload.php" enctype="multipart/form-data" id= "local_selector" method="post" name="local_selector"><div class="form-group">
+      <label class="control-label requiredField">
+       File 
+      </label>
+      <input id="upfile" name="upfile" type="file">
+     </div>
+     <div class="form-group">
+      <div>
+       <button class="btn btn-primary " name="submit" type="submit">
+        Submit File
+       </button>
+      </div>
+     </div>
+    </form>
+</div>
 <?php include 'footer.htm';?>
