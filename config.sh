e4486555b93eea40cee684b4b960c8a656f993c0
steelee <esteele101@gmail.com>
2016-07-19 21:08:18 +0000

    Added proper comments and formatted JS, added new table for groups - config file adds proper CORS headers - JS file fixed and commented - init.sql adding new tables for api groups
    Signed-off-by: steelee <esteele101@gmail.com>

--- config.sh
+++ config.sh
@@ -7,8 +7,10 @@ else
     sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
     a2enmod rewrite
 fi
-a2enmod headers
 echo 'Header always set Access-Control-Allow-Origin "*"' >> /etc/apache2/apache2.conf
+echo 'Header always set Access-Control-Allow-Methods "POST, GET, OPTIONS, DELETE, PUT' >> /etc/apache2/apache2.conf
+echo 'Header always set Access-Control-Allow-Headers "*"' >> /etc/apache2/apache2.conf
+a2enmod headers
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
--- dist/lib/gen-swagger.js
+++ dist/lib/gen-swagger.js
@@ -1,18 +1,11 @@
 /**
- * * gen_swagger(target_url) 
- * * 
- * *
- * * @method methodName
- * * @param {String} foo Argument 1
- * * @param {Object} config A config object
- * * @param {String} config.name The name on the config object
- * * @param {Function} config.callback A callback function on the config object
- * * @param {Boolean} [extra=false] Do extra, optional work
- * * @return {Boolean} Returns true on success
- * 
-*/
+ * Contains helper functions for the entire page and various jQuery triggers
+ * gen_swagger(target_url) renders the swagger-ui given a remote or local path
+ * add_window(form_name) adds the specified form (by ID) to the main container
+ * jQuery functionality to implement search bar and window functionality
+ */
 function gen_swagger(target_url) {
-    if (target_url == "add_api" || target_url=="add_auth") {
+    if (target_url == "add_api" || target_url == "add_auth") {
         add_window(target_url);
     } else {
         document.getElementById("add_api_form").className = "hidden";
@@ -32,71 +25,64 @@ function gen_swagger(target_url) {
             docExpansion: "none",
             jsonEditor: false,
             defaultModelRendering: 'schema',
-	    showRequestHeaders: true
-	    });
-	window.swaggerUi.load();
-	var bearerToken = "Bearer " + "ZO5YnAukfA8OfUi3rciNGceeXM8I";
-	window.swaggerUi.api.clientAuthorizations.remove('api_key');
-	var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("Authorization", bearerToken, "header");
-	window.swaggerUi.api.clientAuthorizations.add('Bearer', apiKeyAuth);
-	console.log("Login Succesfull!");
+            showRequestHeaders: true
+        });
+        window.swaggerUi.load();
     }
 }
 
-function add_window(form_name){
-	document.getElementById("swagger-ui-container").innerHTML = "";
-	var forms = $('label[id*="_form"]');
-	forms.innerHTML = ""; 
-	document.getElementById(form_name.concat("_form")).className = "visible";
+function add_window(form_name) {
+    document.getElementById("swagger-ui-container").innerHTML = "";
+    var forms = $('label[id*="_form"]');
+    forms.innerHTML = "";
+    document.getElementById(form_name.concat("_form")).className = "visible";
 }
 
 $(document).ready(function() {
-		$("#search").keyup(function(){
-				var filter = $(this).val(); // get the value of the input, which we filter on
-				if (filter){
-				$("#listprime").find("a:not(:contains(" + filter + "))").parent().slideUp("fast");
-				$("#listprime").find("a:contains(" + filter + ")").parent().slideDown("fast");
-				}else{
-				$("#listprime").find("li").slideDown();
-				}
-				});
-		$("#api_selector").submit(function() {
-				var url = $("input:first").val();
-				var formData = {
-				'url': url
-				};
-				$.ajax({
-url: "api/add.php",
-global: false,
-type: "POST",
-cache: false,
-datatype: "json",
-data: formData,
-success: function(response) {
-console.log(response);
-}
+    $("#search").keyup(function() {
+        var filter = $(this).val(); // get the value of the input, which we filter on
+        if (filter) {
+            $("#listprime").find("a:not(:contains(" + filter + "))").parent().slideUp("fast");
+            $("#listprime").find("a:contains(" + filter + ")").parent().slideDown("fast");
+        } else {
+            $("#listprime").find("li").slideDown();
+        }
+    });
+    $("#api_selector").submit(function() {
+        var url = $("input:first").val();
+        var formData = {
+            'url': url
+        };
+        $.ajax({
+            url: "api/add.php",
+            global: false,
+            type: "POST",
+            cache: false,
+            datatype: "json",
+            data: formData,
+            success: function(response) {
+                console.log(response);
+            }
+        });
+    });
+    $.ajax({
+        url: "api/populate.php",
+        global: false,
+        type: "POST",
+        cache: false,
+        dataType: "json",
+        success: function(response) {
+            $("#listprime").append('<li id = "add_api"><a href="#"><b>+</b> Add API<i class="fa fa-angle-right"></i></a></li>');
+            $.each(response, function(index) {
+                $("#listprime").append('<li id = "' + response[index].url + '"><a href="#">' + response[index].name + '<i class="fa fa-angle-right"></i></a></li>');
+
+            });
+            $("ul#listprime li").on("click", function() {
+                gen_swagger(($(this).attr('id')));
+                $("li").removeClass("active");
+                $(this).addClass("active");
+            });
+
+        }
+    });
 });
-				});
-$.ajax({
-url: "api/populate.php",
-global: false,
-type: "POST",
-cache: false,
-dataType: "json",
-success: function(response) {
-$("#listprime").append('<li id = "add_api"><a href="#"><b>+</b> Add API<i class="fa fa-angle-right"></i></a></li>');
-$.each(response, function(index) {
-		$("#listprime").append('<li id = "' + response[index].url + '"><a href="#">' + response[index].name + '<i class="fa fa-angle-right"></i></a></li>');
-
-		});
-$("ul#listprime li").on("click", function() {
-		gen_swagger(($(this).attr('id')));
-		$("li").removeClass("active");
-		$(this).addClass("active");
-		});
-
-}
-});
-
-});
-
