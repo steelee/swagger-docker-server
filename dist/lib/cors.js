f76038cbf30a44e090441d23b6592537177d030e
steelee <esteele101@gmail.com>
2016-08-18 23:43:23 +0000

    Fixed formatting on files - indentation fixed

--- dist/lib/cors.js
+++ dist/lib/cors.js
@@ -1,51 +1,53 @@
 // Create the XHR object.
 function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
 }
 
 // Make the actual CORS request.
 function makeCorsRequest(url, key, callback) {
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    console.log('CORS not supported');
-    return;
-  }
-  xhr.setRequestHeader('Authorization', key);
-  xhr.setRequestHeader('Accept', "application/json");
-  // Response handlers.
-  xhr.onload = function() {
-    try {
-    	var text = JSON.parse(xhr.responseText);
-    }catch(e){
-	$.ajax({
-		url: "api/create.php",
-		global: "false",
-		type: "POST",
-		cache: "false",
-		data: {"data" : xhr.responseText},
-		success: function(response){
-			callback(response);
-		}
-	});
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        console.log('CORS not supported');
+        return;
     }
-    callback(text);
-  };
+    xhr.setRequestHeader('Authorization', key);
+    xhr.setRequestHeader('Accept', "application/json");
+    // Response handlers.
+    xhr.onload = function() {
+        try {
+            var text = JSON.parse(xhr.responseText);
+        } catch (e) {
+            $.ajax({
+                url: "api/create.php",
+                global: "false",
+                type: "POST",
+                cache: "false",
+                data: {
+                    "data": xhr.responseText
+                },
+                success: function(response) {
+                    callback(response);
+                }
+            });
+        }
+        callback(text);
+    };
 
-  xhr.onerror = function() {
-    console.log('Woops, there was an error making the request.');
-  };
-  
-  xhr.send();
+    xhr.onerror = function() {
+        console.log('Woops, there was an error making the request.');
+    };
+
+    xhr.send();
 }
--- dist/swaggerhub.php
+++ dist/swaggerhub.php
@@ -1,37 +1,37 @@
 <?php include "header.htm"?>
 <div class="col-sm-3 col-md-3 sidebar">
-   <h4>SwaggerHub APIs</h4>
-	<script>
-	values = {};
-	makeCorsRequest(getCookie("swaggercookie_url"), getCookie("swaggercookie_key"), function(val){
-        	for (var count = 0; count < Object.keys(val['apis']).length; count ++){
-			var private_item = val['apis'][count]["properties"].length - 1;
-			$("#swaggerhub-list").append('<div class="list-group-item" id = "' + val['apis'][count]["name"] + '_' + val['apis'][count]["properties"][private_item]["value"] + '">' + val['apis'][count]["name"] + '<i class="fa fa-angle-right"></i></div>');
-			values[val['apis'][count]["name"]] = JSON.stringify(val['apis'][count]);
-			
-		}
-	var on_load = function(){
-    $("#swaggerhub-list div").on("click", function(){
-        $("div").removeClass("active");
-        $(this).addClass("active");
-        var dataID = $(this).text();
-	gen_swaggerhub(values[dataID]);
-    }); 
-}
+    <h4>SwaggerHub APIs</h4>
+    <script>
+        values = {};
+        makeCorsRequest(getCookie("swaggercookie_url"), getCookie("swaggercookie_key"), function(val) {
+            for (var count = 0; count < Object.keys(val['apis']).length; count++) {
+                var private_item = val['apis'][count]["properties"].length - 1;
+                $("#swaggerhub-list").append('<div class="list-group-item" id = "' + val['apis'][count]["name"] + '_' + val['apis'][count]["properties"][private_item]["value"] + '">' + val['apis'][count]["name"] + '<i class="fa fa-angle-right"></i></div>');
+                values[val['apis'][count]["name"]] = JSON.stringify(val['apis'][count]);
 
-$(document).ready(on_load);
-$(window).bind("page:change",on_load);
+            }
+            var on_load = function() {
+                $("#swaggerhub-list div").on("click", function() {
+                    $("div").removeClass("active");
+                    $(this).addClass("active");
+                    var dataID = $(this).text();
+                    gen_swaggerhub(values[dataID]);
+                });
+            }
+
+            $(document).ready(on_load);
+            $(window).bind("page:change", on_load);
 
 
-	});
-	</script>
-   <ul class="nav nav-sidebar" id="swaggerhub-list">
-   </ul>
+        });
+    </script>
+    <ul class="nav nav-sidebar" id="swaggerhub-list">
+    </ul>
 </div>
 
 
 <div class="col-sm-9 col-sm-offset-4 col-md-9 col-md-offset-3 main swagger-ui-wrap">
-<div class="swagger-ui-container" id="swagger-ui-container">
-</div>
+    <div class="swagger-ui-container" id="swagger-ui-container">
+    </div>
 </div>
 <?php include "footer.htm"?>
