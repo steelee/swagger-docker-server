0129eb51bbef9419a74236ca6ade122ce5b6241c
Evan Steele <esteele101@gmail.com>
2016-09-28 00:13:49 +0000

    formatting change to window swagger file

--- dist/lib/gen-swagger.js
+++ dist/lib/gen-swagger.js
@@ -98,110 +98,110 @@ $('#swagger-ui-container').bind('DOMSubtreeModified', function() {
 });
 
 $(document).ready(function() {
-    $.getScript("/config.js", function(){
-    $("#dialog").dialog({
-        autoOpen: false,
-        modal: true,
-        show: "blind",
-        hide: "blind",
-        "width": 750,
-    });
-    var target_API = getParameterByName('api');
-    $("#search").keyup(function() {
-        var filter = $(this).val(); // get the value of the input, which we filter on
-        if (filter) {
-            $("#listprime li ul").find("div:not(:contains(" + filter + "))").slideUp("fast");
-            $("#listprime li ul").find("div:contains(" + filter + ")").slideDown("fast");
-        } else {
-            $("#listprime").find("div").slideDown();
-        }
-    });
-    $.ajax({
-        data: {
-            'cmd': 'unique_group'
-        },
-        url: "/api/populate.php",
-        global: false,
-        type: "POST",
-        cache: false,
-        dataType: "json",
-        success: function(response) {
-            $.each(response, function(index) {
-                if (response[index].api_group != "No_Group") {
-                    $("#listprime").prepend('<li class="data-dropdown"><ul class="nav nav-pills nav-stacked collapse in" id="' + response[index].api_group + '"><li data-toggle="collapse" data-parent="#' + response[index].api_group + '" href="#' + response[index].api_group + '_target"><a class="nav-sub-container">' + response[index].api_group + ' <div class="caret-container"><span class="caret arrow"></span></div></a></li><ul class="nav nav-pills nav-stacked collapse " id="' + response[index].api_group + '_target"></ul></ul>');
-                }
-            });
-            $("#menu_bar").append('<h3>Unassigned</h3><ul class="nav nav-pills nav-stacked" id="no_domain"></ul><button type="button" class="btn btn-secondary"><span class="glyphicon glyphicon-plus"></span> Add Domain</button>');
-            $.ajax({
-                url: "https://api.github.com/repos/steelee/swagger-docker-server/tags",
-                global: false,
-                type: "GET",
-                cache: false,
-                dataType: "json",
-                success: function(git) {
-                    for (var key in git) {
-                        if (git.hasOwnProperty(key)) {
-                            if (git[key]["name"].indexOf(config.env) != -1) {
-				var curr_key = git[key];
-				break;
-                            	}
-                        }
+    $.getScript("/config.js", function() {
+        $("#dialog").dialog({
+            autoOpen: false,
+            modal: true,
+            show: "blind",
+            hide: "blind",
+            "width": 750,
+        });
+        var target_API = getParameterByName('api');
+        $("#search").keyup(function() {
+            var filter = $(this).val(); // get the value of the input, which we filter on
+            if (filter) {
+                $("#listprime li ul").find("div:not(:contains(" + filter + "))").slideUp("fast");
+                $("#listprime li ul").find("div:contains(" + filter + ")").slideDown("fast");
+            } else {
+                $("#listprime").find("div").slideDown();
+            }
+        });
+        $.ajax({
+            data: {
+                'cmd': 'unique_group'
+            },
+            url: "/api/populate.php",
+            global: false,
+            type: "POST",
+            cache: false,
+            dataType: "json",
+            success: function(response) {
+                $.each(response, function(index) {
+                    if (response[index].api_group != "No_Group") {
+                        $("#listprime").prepend('<li class="data-dropdown"><ul class="nav nav-pills nav-stacked collapse in" id="' + response[index].api_group + '"><li data-toggle="collapse" data-parent="#' + response[index].api_group + '" href="#' + response[index].api_group + '_target"><a class="nav-sub-container">' + response[index].api_group + ' <div class="caret-container"><span class="caret arrow"></span></div></a></li><ul class="nav nav-pills nav-stacked collapse " id="' + response[index].api_group + '_target"></ul></ul>');
                     }
-                                $.ajax({
-                                    url: curr_key["commit"].url,
-                                    global: false,
-                                    type: "GET",
-                                    cache: false,
-                                    dataType: "json",
-                                    success: function(tag) {
-                                        $("#menu_bar").append('<div style="position:absolute;bottom:5%"><h3>' + config.env.toUpperCase() + '</h3><a target="_blank" href="' + tag.html_url + '">' + curr_key["name"] + '</a></div>');
-                                    }
+                });
+                $("#menu_bar").append('<h3>Unassigned</h3><ul class="nav nav-pills nav-stacked" id="no_domain"></ul><button type="button" class="btn btn-secondary"><span class="glyphicon glyphicon-plus"></span> Add Domain</button>');
+                $.ajax({
+                    url: "https://api.github.com/repos/steelee/swagger-docker-server/tags",
+                    global: false,
+                    type: "GET",
+                    cache: false,
+                    dataType: "json",
+                    success: function(git) {
+                        for (var key in git) {
+                            if (git.hasOwnProperty(key)) {
+                                if (git[key]["name"].indexOf(config.env) != -1) {
+                                    var curr_key = git[key];
+                                    break;
+                                }
+                            }
+                        }
+                        $.ajax({
+                            url: curr_key["commit"].url,
+                            global: false,
+                            type: "GET",
+                            cache: false,
+                            dataType: "json",
+                            success: function(tag) {
+                                $("#menu_bar").append('<div style="position:absolute;bottom:5%"><h3>' + config.env.toUpperCase() + '</h3><a target="_blank" href="' + tag.html_url + '">' + curr_key["name"] + '</a></div>');
+                            }
 
-                                });
-                }
-            });
-            $.ajax({
-                data: {
-                    'cmd': 'group'
-                },
-                url: "/api/populate.php",
-                global: false,
-                type: "POST",
-                cache: false,
-                dataType: "json",
-                success: function(response) {
-                    $.each(response, function(index) {
-                        if (response[index].api_group != "No_Group") {
-                            $("ul#" + response[index].api_group + " ul").append('<div class="list-group-item" id="' + response[index].url + '">' + response[index].name + '</div>');
+                        });
+                    }
+                });
+                $.ajax({
+                    data: {
+                        'cmd': 'group'
+                    },
+                    url: "/api/populate.php",
+                    global: false,
+                    type: "POST",
+                    cache: false,
+                    dataType: "json",
+                    success: function(response) {
+                        $.each(response, function(index) {
+                            if (response[index].api_group != "No_Group") {
+                                $("ul#" + response[index].api_group + " ul").append('<div class="list-group-item" id="' + response[index].url + '">' + response[index].name + '</div>');
+                            } else {
+                                $("#no_domain").append('<div class="list-group-item" id="' + response[index].url + '">' + response[index].name + '</div>');
+                            }
+                        });
+                        $("ul#listprime li ul ul div, #no_domain div").on("click", function() {
+                            var newURL = updateURLParameter(window.location.href, 'api', ($(this).text()));
+                            window.history.replaceState({}, 'title', newURL);
+                            var url = SwaggerWindow($(this).attr('id'), $(this).text());
+                            url.gen_swagger(url.target_URL);
+                            $("li").removeClass("active");
+                            $("div").removeClass("active");
+                            $(this).addClass("active");
+                        });
+
+                        if (target_API != null) {
+                            $('ul#listprime li ul div:contains("' + target_API + '")').trigger("click");
+                            $('#no_domain div:contains("' + target_API + '")').trigger("click");
                         } else {
-                            $("#no_domain").append('<div class="list-group-item" id="' + response[index].url + '">' + response[index].name + '</div>');
+                            $("#swagger-ui-container").load("views/graphs.htm");
                         }
-                    });
-                    $("ul#listprime li ul ul div, #no_domain div").on("click", function() {
-                        var newURL = updateURLParameter(window.location.href, 'api', ($(this).text()));
-                        window.history.replaceState({}, 'title', newURL);
-                        var url = SwaggerWindow($(this).attr('id'), $(this).text());
-                        url.gen_swagger(url.target_URL);
-                        $("li").removeClass("active");
-                        $("div").removeClass("active");
-                        $(this).addClass("active");
-                    });
-
-                    if (target_API != null) {
-                        $('ul#listprime li ul div:contains("' + target_API + '")').trigger("click");
-                        $('#no_domain div:contains("' + target_API + '")').trigger("click");
-                    } else {
-                        $("#swagger-ui-container").load("views/graphs.htm");
                     }
-                }
-            });
+                });
 
-        }
-    });
-    $("#add_api").on("click", function() {
-        var url = SwaggerWindow($(this).attr('id'), null);
-        url.gen_swagger(url.target_URL);
-    });
+            }
+        });
+        $("#add_api").on("click", function() {
+            var url = SwaggerWindow($(this).attr('id'), null);
+            url.gen_swagger(url.target_URL);
+        });
 
-});
+    });
 });
